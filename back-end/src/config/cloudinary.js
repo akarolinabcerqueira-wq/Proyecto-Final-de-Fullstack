329fb2f4c00ea52e75b92a59d25e9bae75e1a249
armitayati <akarolinabcerqueira@alumnos.cei.es>
2026-02-22 21:16:10 +0330

    comments

--- back-end/src/config/cloudinary.js
+++ back-end/src/config/cloudinary.js
@@ -3,12 +3,20 @@ dotenv.config();
 
 import { v2 as cloudinary } from "cloudinary";
 
+/* --------------------------------------------------
+   CONFIGURACIÓN DE CLOUDINARY
+   Se cargan las credenciales desde variables de entorno
+-------------------------------------------------- */
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+/* Log opcional para verificar configuración */
 console.log("Cloudinary config:", cloudinary.config());
 
+/* --------------------------------------------------
+   EXPORTAR INSTANCIA CONFIGURADA
+-------------------------------------------------- */
 export default cloudinary;
